dc59d168a7bc3f4010a8eee86c8a5095071b4f1d
Tong Liu <>
2020-07-16 09:59:03 -0400

    First iteration on digital bank

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,13 @@
 package org.xpdojo.bank;
 
 public class Account {
+    double balance;
+
+    Account() {
+        balance = 0;
+    }
+
+    public double balance() {
+        return balance;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -4,12 +4,20 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AccountTest {
 
+    @Test
+    public void newAccountWithZeroBalance() {
+        Account account = new Account();
+        assertEquals(account.balance(), 0);
+    }
+    /*
     @Test
 	@Disabled
     public void depositAnAmountToIncreaseTheBalance() {
         assertThat("your first test").isBlank();
     }
+    */
 }
